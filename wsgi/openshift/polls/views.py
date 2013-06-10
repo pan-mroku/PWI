a774ac52fb94e2ff9074d4948a53a547de800881
nobody <bierkionline@gmail.com>
2013-06-10 20:17:10 +0200

    part 4 wip

--- wsgi/openshift/polls/views.py
+++ wsgi/openshift/polls/views.py
@@ -1,7 +1,9 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
-from django.template import Context, loader
-from polls.models import Poll
-from django.http import HttpResponse
+from django.template import Context, loader, RequestContext
+from models import Poll, Choice
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by("-pub_date")[:5]
@@ -9,12 +11,28 @@ def index(request):
 
 def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response("polls/detail.html", {"poll": p})
+    return render_to_response("polls/detail.html", {"poll": p}, context_instance=RequestContext(request))
 
 
 def results(request, poll_id):
-    return HttpResponse("Youre looking at the results of poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response("polls/results.html", {"poll": p})
 
 
 def vote(request, poll_id):
-    return HttpResponse("Youre voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render_to_response("polls/detail.html", {
+        "poll": p,
+        "error_message": "You didnt select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls.views.results", args=(p.id,)))
