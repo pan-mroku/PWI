3b0c8f52e04555f003d44c83bb9fe5739d43dae1
nobody <bierkionline@gmail.com>
2013-06-16 23:11:12 +0200

    proba na os

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='PWI EGZ',
     version='1.0',
     description='Egzamin z PWI',
-    author='Your Name',
+    author='Moje Imie',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=['Django<=1.4','django-celery','django-kombu','django-registration','requests'],
